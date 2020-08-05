58268ea54dea3705a53c9987e24d9e0758e46933
Ben <bwc9876@gmail.com>
2020-08-05 11:44:56 -0400

    Update setup.py url

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='BeLib',
     version='1.0.0',
     packages=find_packages(),
-    url='None',
+    url='https://github.com/Bwc9876/BLib',
     license='"License :: OSI Approved :: MIT License"',
     author='bwc9876',
     author_email='bwc9876@gmail.com',
-    description='A collection fo personal tools'
+    description='A collection of personal tools'
 )
